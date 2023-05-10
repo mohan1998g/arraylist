50ae771f46326e65a82e7bd0bde5cd970cf2326a
msn98leo <gokavarapumohan98@gmail.com>
2023-05-10 19:17:38 +0530

    Initial commit

---
+++ ArrayListOp1.java
@@ -0,0 +1,18 @@
+package javaDSArrayList;
+
+import java.util.ArrayList; // Importing the ArrayList class
+import java.util.List;
+
+public class ArrayListOp1 {
+
+	public static void main(String[] args) {
+		List<String> food = new ArrayList<String>(); // Creating a list of String elements
+		food.add("Pizza"); // Adding elements
+		food.add("Burger");
+		food.add("Pasta");
+		food.add("Sandwich");
+		System.out.println("Food items: " + food);
+
+	}
+
+}
